881195f96cc5f063d488a338d5009706149e782f
NoCredits <rene@vanderweide.com>
2020-02-01 20:50:29 +0100

    next

--- app/src/main/java/com/vanderweide/mybub/MainActivity.java
+++ app/src/main/java/com/vanderweide/mybub/MainActivity.java
@@ -1,14 +1,25 @@
 package com.vanderweide.mybub;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Set fullscreen
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Set No Title
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+
+        this.setContentView(new GameSurface(this));
     }
+
 }
