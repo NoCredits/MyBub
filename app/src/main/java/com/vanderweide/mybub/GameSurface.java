a1599b86210dd85a7e4ec76bdae6f899b17bc67d
NoCredits <rene@vanderweide.com>
2020-02-04 22:30:11 +0100

    All use gameobject

--- app/src/main/java/com/vanderweide/mybub/GameSurface.java
+++ app/src/main/java/com/vanderweide/mybub/GameSurface.java
@@ -5,18 +5,13 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.util.SparseArray;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Random;
 
 public class GameSurface extends SurfaceView implements SurfaceHolder.Callback {
 
@@ -25,18 +20,12 @@ public class GameSurface extends SurfaceView implements SurfaceHolder.Callback {
     private List<ChibiCharacter> chibiList = new ArrayList<ChibiCharacter>();
     private List<Explosion> explosionList = new ArrayList<Explosion>();
     private List<GameObject> gameList = new ArrayList<GameObject>();
-    private List<GameObject> zList = new ArrayList<GameObject>();
-    private List<GameObject> layerList = new ArrayList<GameObject>();
-
-    private SparseArray <ArrayList<GameObject>> layerArray= new SparseArray<>();
-//    private final HashMap<Integer, ArrayList<Integer>> layerMap = new HashMap<Integer, ArrayList<Integer>>();
 
     private int gridRows=11;
     private int gridCols=11;
     private GridObject grid=new GridObject(gridRows,gridRows);
 
 
-    private static Random rand;
 
 
     public GameSurface(Context context)  {
@@ -50,9 +39,7 @@ public class GameSurface extends SurfaceView implements SurfaceHolder.Callback {
     }
 
     public void update()  {
-        for(ChibiCharacter chibi: chibiList) {
-            chibi.update();
-        }
+
         for(Explosion explosion: this.explosionList)  {
             explosion.update();
         }
@@ -75,23 +62,6 @@ public class GameSurface extends SurfaceView implements SurfaceHolder.Callback {
         }
     }
 
-    public static int randInt(int min, int max) {
-
-        // NOTE: This will (intentionally) not run as written so that folks
-        // copy-pasting have to think about how to initialize their
-        // Random instance.  Initialization of the Random instance is outside
-        // the main scope of the question, but some decent options are to have
-        // a field that is initialized once and then re-used as needed or to
-        // use ThreadLocalRandom (if using at least Java 1.7).
-        //
-        // In particular, do NOT do 'Random rand = new Random()' here or you
-        // will get not very good / not very random results.
-        // nextInt is normally exclusive of the top value,
-        // so add 1 to make it inclusive
-        int randomNum = rand.nextInt((max - min) + 1) + min;
-
-        return randomNum;
-    }
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
@@ -131,52 +101,41 @@ public class GameSurface extends SurfaceView implements SurfaceHolder.Callback {
     public void draw(Canvas canvas)  {
         super.draw(canvas);
 
-        for(ChibiCharacter chibi: chibiList)  {
-            chibi.draw(canvas);
-        }
-
-
 
         for(Explosion explosion: this.explosionList)  {
             explosion.draw(canvas);
         }
 
-//        for(GameObject obj: this.gameList)  {
-//
-//            obj.draw(canvas);
-//        }
 
-
-        Utils.drawLayers(canvas,layerArray);
+        Utils.drawLayers(canvas,gameList);
     }
 
     // Implements method of SurfaceHolder.Callback
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
 
-        rand=new Random();
+        //rand=new Random();
         Bitmap chibiBitmap1 = BitmapFactory.decodeResource(this.getResources(),R.drawable.chibi1);
         ChibiCharacter chibi1 = new ChibiCharacter(this,chibiBitmap1,100,50);
 
         Bitmap chibiBitmap2 = BitmapFactory.decodeResource(this.getResources(),R.drawable.chibi2);
         ChibiCharacter chibi2 = new ChibiCharacter(this,chibiBitmap2,300,150);
-       this.chibiList.add(chibi1);
-        this.chibiList.add(chibi2);
 
         for (int r=0;r<gridRows;r++){
             for (int c=0;c<gridCols;c++) {
 
-                Hexagon hex=new Hexagon(this,30,randInt(25,this.getWidth()-25),randInt(25,this.getHeight()-25),Color.BLUE);
-                hex.setMovingVector(randInt(-10,10),randInt(-10,10));
+                Hexagon hex=new Hexagon(this,30,Utils.randInt(25,this.getWidth()-25),Utils.randInt(25,this.getHeight()-25),Color.BLUE);
+                hex.setMovingVector(Utils.randInt(-10,10),Utils.randInt(-10,10));
                 //hex.setMovingVector(0,0);
 //            hex.setColor(Color.rgb(randInt(0,255),randInt(0,255),randInt(0,255)));
 
 
                 hex.setRadius((this.getWidth()/(gridCols+1)/2));
-                hex.setRadius(randInt(30,70));
-                hex.setVelocity(rand.nextFloat()/3);
-                hex.setZ_index(randInt(0,4));
-                switch (hex.getZ_index()){
+                hex.setRadius(Utils.randInt(30,70));
+                hex.setVelocity(Utils.rand.nextFloat()/3);
+                hex.setZ_index(2);
+                hex.setLayer(Utils.randInt(0,5));
+                switch (hex.getLayer()){
                     case 0: hex.setColor(Color.GREEN);
                         break;
                     case 1: hex.setColor(Color.RED);
@@ -187,13 +146,16 @@ public class GameSurface extends SurfaceView implements SurfaceHolder.Callback {
                         break;
                     case 4: hex.setColor(Color.YELLOW);
                         break;
+                    case 5: hex.setColor(Color.MAGENTA);
+                        break;
                 }
-                int ran=randInt(0,3);
+                int ran=Utils.randInt(0,3);
                if (ran>0){
 
                    grid.add(r,c,hex);
                    gameList.add(hex);
-                   layerArray=Utils.addToArray(layerArray,hex.getZ_index(),hex);
+
+
 
                    hex.calculateGridPosToScreen(grid.getPadding());
 
@@ -201,6 +163,17 @@ public class GameSurface extends SurfaceView implements SurfaceHolder.Callback {
             }
         }
 
+        chibi1.setVelocity(Utils.rand.nextFloat()/3);
+        chibi1.setMovingVector(Utils.randInt(-10,10),Utils.randInt(-10,10));
+        chibi1.setLayer(3);
+        chibi1.setZ_index(3);
+        gameList.add(chibi1);
+
+        chibi2.setVelocity(Utils.rand.nextFloat()/3);
+        chibi2.setMovingVector(Utils.randInt(-10,10),Utils.randInt(-10,10));
+        chibi2.setLayer(3);
+        chibi2.setZ_index(3);
+        gameList.add(chibi2);
 
 
         this.gameThread = new GameThread(this,holder);
